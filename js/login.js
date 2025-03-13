3fdd48e9033721638a4ea62bae283bd891b9f650
nayeem19999 <nhnayeem13@gmail.com>
2025-03-13 13:32:53 +0600

    first commit for setup the project

---
+++ js/login.js
@@ -0,0 +1,13 @@
+document.getElementById('submit-button').addEventListener('click', function () {
+    //email
+    const emailInput = document.getElementById('user-email');
+    const email = emailInput.value;
+
+    // password
+    const password = document.getElementById('user-password').value;
+    // add
+
+
+
+
+})
